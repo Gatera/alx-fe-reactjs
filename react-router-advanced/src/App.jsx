b225022ee7a9fd592f7d9f49a4634b2d77aad9e9
Gatera <vgatera@gmail.com>
2024-09-01 23:51:54 +0200

    added BrowserRouter in App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,20 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import Profile from './pages/profile';
-import ProtectedRoute from './Components/ProtectedRoute';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile/*" element={
-        <ProtectedRoute isAuthenticated={isAuthenticated}>
-          <Profile />
-        </ProtectedRoute>
-      } />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile/*" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
