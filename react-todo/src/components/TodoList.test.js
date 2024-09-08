e89f9748443e1c0843ec6af59208b8cc6448cf01
Gatera <vgatera@gmail.com>
2024-09-08 14:38:01 +0200

    moved test file to components

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'; // Ensure jest-dom is installed
-import TodoList from '../components/TodoList';
+import TodoList from './TodoList';
 
 // Test initial render
 test('renders TodoList component with initial todos', () => {
