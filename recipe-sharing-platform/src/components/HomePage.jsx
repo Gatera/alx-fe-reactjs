c8f3ee324353ccb833f5f4c7a0248ca1bd03c655
Gatera <vgatera@gmail.com>
2024-09-10 14:03:38 +0200

    moved all recipe code to homepage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -24,7 +24,15 @@ function HomePage() {
     <div>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
         {recipes.map((recipe) => (
-          <Recipe recipe={recipe} key={recipe.id} />
+          <div className='w-full bg-white shadow-sm rounded-lg text-left hover:shadow-lg duration-300'>
+            <div className='w-full'>
+              <div style={{backgroundImage: `url(${recipe.image})`}} className='w-full h-40 rounded-t-lg bg-no-repeat bg-center bg-cover'></div>
+            </div>
+            <div className='p-5'>
+              <h3 className='text-lg'>{recipe.title}</h3>
+              <p className='text-sm text-gray-600 mt-2'>{recipe.summary}</p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
