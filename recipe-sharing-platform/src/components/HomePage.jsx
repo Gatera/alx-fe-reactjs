01891e5f131927a8b33ac346433b7697cc97292c
Gatera <vgatera@gmail.com>
2024-09-10 13:58:39 +0200

    moved all code from app to homepage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Recipe from './Recipe'
-import recipes from "../data/recipes.json"
+
 
 function HomePage() {
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    //Fetching data from the data.json file
+    const fetchRecipes = async () => {
+      try {
+        const response = await fetch('/data/recipes.json')
+        const data = await response.json()
+        setRecipes(data)
+      } catch (error) {
+        console.error("Error loading recipes:", error)
+      }
+    }
+
+    fetchRecipes()
+  }, [])
+
   return (
     <div>
-        <div>
-            {recipes.map((recipe) => {
-                <Recipe recipe={recipe} key={recipe.id} />
-            })}
-        </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {recipes.map((recipe) => (
+          <Recipe recipe={recipe} key={recipe.id} />
+        ))}
+      </div>
     </div>
   )
 }
